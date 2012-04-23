bb06c4076b8c2370d2d8a5b137a7e5bcd0d86b55
Aleksey V Zapparov <ixti@member.fsf.org>
2012-04-23 02:48:57 +0200

    Add test stubs

---
+++ test/test_index.js
@@ -0,0 +1,19 @@
+/*global describe, it, before, after, beforeEach, afterEach*/
+
+
+'use strict';
+
+
+// stdlib
+var assert = require('assert');
+
+
+// internal
+var Trail = require('../lib/hike/trail');
+
+
+describe('Index', function () {
+  function new_trail() {
+    return new Trail();
+  }
+});
